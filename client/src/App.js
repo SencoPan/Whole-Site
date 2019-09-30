c7d42a1da9d87224bf89777774bef9f57fb121d9
Senco <sarprolts@gmail.com>
2019-09-30 11:28:40 +0300

    Redux implemention
    1q

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import AppNavbar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css.min/css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        <h1> Hello world </h1>
+        <AppNavbar />
+        <ShoppingList />
     </div>
   );
 }
