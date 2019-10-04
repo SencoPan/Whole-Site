f22765caf06353ea451224db39065293df4370c8
Senco <sarprolts@gmail.com>
2019-10-04 17:59:53 +0300

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import ShoppingList from './components/ShoppingList';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 function App() {
   return (
-    <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-    </div>
+     <Provider store={store}>
+        <div className="App">
+            <AppNavbar />
+            <ShoppingList />
+        </div>
+     </Provider>
   );
 }
 
---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,7 @@
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from "./type";
+
+export const getItems = () => {
+    return {
+        type: GET_ITEMS
+    }
+}
